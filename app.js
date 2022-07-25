3b299bbee7c185e14a1b8c855775e35b65f219d6
Juandaya <juandaya2013@gmail.com>
2022-07-25 22:42:18 +0100

    primer app

---
+++ app.js
@@ -0,0 +1,73 @@
+const movies = require("./movies")
+
+const moviesDH = {
+    movies,
+
+    listMovies: function(){
+        movies.forEach(function(movie, index){
+            console.log((index+1) + " " + movie.title)
+        })
+    },
+
+    searchMovies: function(idOrTitle){
+        const peliculaEncontrada = movies.find(function(movie){
+            return ((movie.id == idOrTitle) || (movie.title == idOrTitle)) 
+        })
+        if(peliculaEncontrada){
+            return peliculaEncontrada
+        }else{
+            return null
+        }
+    },
+
+    searchMoviesByGenre: function(genero){
+        const filtradoPorGenero = movies.filter(function(movie){
+            return movie.genre.indexOf(genero) >= 0
+        })
+        if(filtradoPorGenero.length == 0){
+            return null 
+        }
+        else{
+            return filtradoPorGenero
+        }
+          
+    },
+
+    listaDePrecios: function(precio){
+        const total = []
+
+        movies.forEach(function(movie){
+            if (movie.price > 0){
+                total.push(movie.price)
+            }
+        })
+        return total
+    },
+
+    totalPrice: function(){
+        let listaDePrecios = this.listaDePrecios()
+
+        const costoTotal = listaDePrecios.reduce(function(acum, precio){
+            return acum + precio
+        }, 0)
+        return costoTotal
+    },
+
+    changeMovieGenre: function(id, generoViejo, nuevoGenero){
+        let peliculaEncontrada = this.searchMovies(id)
+        let arrayGeneroViejo = peliculaEncontrada.genre // Localizacion del array del genero
+        let indice = arrayGeneroViejo.indexOf(generoViejo)
+        
+        arrayGeneroViejo[indice] = nuevoGenero
+        return peliculaEncontrada
+        
+    }
+}
+
+//moviesDH.listMovies();
+//console.log(moviesDH.searchMovies(04));
+//console.log(moviesDH.searchMoviesByGenre("Accion"));
+//console.log(moviesDH.totalPrice());
+//console.log(moviesDH.changeMovieGenre(02, "Accion", "Terror"))
+
+
---
+++ movies.js
@@ -0,0 +1,49 @@
+const pelisFavoritas = [
+    {
+        id: 01,
+        title: "Focus",
+        rating: 4.2,
+        awards: ["Golden trailer"],
+        length: 104, 
+        price: 3.49,
+        genre: ["Comedia", "Accion", "Suspenso", "Aventuras"]
+    },
+    {
+        id: 02,
+        title: "Diamante de sangre",
+        rating: 4.8,
+        awards: ["National board"],
+        length: 143,
+        price: 3.49,
+        genre: ["Accion", "Aventuras", "Suspenso"]
+    },
+    {
+        id: 03,
+        title: "Infiltrados",
+        rating: 4.6,
+        awards: ["Golden globe", "MTV","Phoenix Film" ],
+        length: 151,
+        price: 2.49,
+        genre: ["Accion", "Aventuras", "Suspenso"]
+    },
+    {
+        id: 04,
+        title: "In Time",
+        rating: 4.3,
+        awards: [],
+        length: 109,
+        price: 2.49,
+        genre: ["Accion", "Aventuras", "Ciencia ficcion", "Suspenso"]
+    },
+    {
+        id: 05,
+        title: "El diario de una motocicleta",
+        rating: 4.8,
+        awards: ["Goya"],
+        length: 126,
+        price: 3.49,
+        genre: ["Aventura", "Suspenso"] 
+    }
+]
+
+module.exports = pelisFavoritas
